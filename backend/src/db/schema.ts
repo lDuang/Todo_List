e381bfa6188b201811bdbebdcaa9bd76f75ec077
duang <duang7896@163.com>
2025-10-04 08:05:06 +0000

    feat-添加数据库限制title长度

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -5,7 +5,7 @@ export const todos = pgTable('todos', {
   // id 自增,主键约束
   id: serial('id').primaryKey(),
   // title 文本 不为空(notNull) 唯一(unique)
-  title: varchar('title', { length: 255 }).notNull().unique(),
+  title: varchar('title', { length: 64 }).notNull().unique(),
   // completed 布尔值 默认 false 不为空
   completed: boolean('completed').default(false).notNull(),
   // created_at 时间 默认当前时间 不为空 
